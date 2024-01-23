020bedaec65e4220c569b39ca0aa770b4c56aa2a
msenjalia <144574835+msenjalia@users.noreply.github.com>
2024-01-23 15:08:12 -0500

    AI integration

--- src/script.js
+++ src/script.js
@@ -1,11 +1,27 @@
-function generateSearchResults(event) {
-  event.preventDefault();
+function displayResults(response) {
+  console.log(response.data);
 
   new Typewriter("#search-city-info", {
-    strings: ["Hello", "World"],
+    strings: response.data.answer,
     autoStart: true,
+    delay: 1,
   });
 }
 
+function generateSearch(event) {
+  event.preventDefault();
+
+  let searchQuery = document.querySelector(".searchBox");
+  let searchText = searchQuery.value;
+
+  let prompt = `${searchText}`;
+  let context =
+    "are an expert travel guide who specializes in setting a itenarary for a solo traveler. please give the ideal itenaray for each day in the city. answer format should list the <h2> City, Country </h2>, new line give the daily itenerary in html and bullet list for each day.";
+  let apiKey = "400a01208e33f9b5ecot8677e1d71bfe";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayResults);
+}
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", generateSearchResults);
+searchForm.addEventListener("submit", generateSearch);
