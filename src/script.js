a09ea0f399fd84d9e6b82bfc41a6020d6c8794fa
msenjalia <144574835+msenjalia@users.noreply.github.com>
2024-01-23 19:17:31 -0500

    api prompt and context

--- src/script.js
+++ src/script.js
@@ -1,11 +1,10 @@
 function displayResults(response) {
   console.log(response.data);
 
-  new Typewriter("#search-city-info", {
-    strings: response.data.answer,
-    autoStart: true,
-    delay: 1,
-  });
+  let itinerary = response.data.answer;
+
+  let cityItinerary = document.querySelector("#search-city-info");
+  cityItinerary.innerHTML = itinerary;
 }
 
 function generateSearch(event) {
@@ -14,13 +13,21 @@ function generateSearch(event) {
   let searchQuery = document.querySelector(".searchBox");
   let searchText = searchQuery.value;
 
-  let prompt = `${searchText}`;
+  let prompt =
+    `${searchText}` +
+    "answer format should list the <h2> City, Country </h2>, new line give the daily itenerary in html and bullet list for each day, day in bold letters.";
   let context =
-    "are an expert travel guide who specializes in setting a itenarary for a solo traveler. please give the ideal itenaray for each day in the city. answer format should list the <h2> City, Country </h2>, new line give the daily itenerary in html and bullet list for each day.";
+    "are an expert travel guide who specializes in setting 2 day maximum itineraries set as morning, afternoon and evening. You like to explore local cultures and current events, vegetarian food, shopping and adventures, be specific and detailed in possible.";
   let apiKey = "400a01208e33f9b5ecot8677e1d71bfe";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayResults);
+
+  new Typewriter("#search-city-info", {
+    strings: ["Preparing your itinerary"],
+    autoStart: true,
+    cursor: "",
+  });
 }
 
 let searchForm = document.querySelector("#search-form");
